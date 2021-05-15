59f8d5a6d6792a3963ac9875400f6809be86e0a0
Leon <shi406406@qq.com>
2021-05-15 23:59:15 +0800

    feat:optimize clash parser

--- src/test/java/me/leon/YamlTest.kt
+++ src/test/java/me/leon/YamlTest.kt
@@ -10,22 +10,17 @@ class YamlTest {
     fun yamlTest() {
 
         var y = "E:\\github\\Sub\\sub\\sub.yml"
-        val url = "http://buliang0.tk/tool/freeproxy/05-06/clash-27.yml".readFromNet()
+//        val url = "http://buliang0.tk/tool/freeproxy/05-06/clash-27.yml".readFromNet()
+        val url = "https://suo.yt/E80gdbo".readFromNet()
 
-        Constructor(Clash::class.java)
-//            .apply {
-//            addTypeDescription(TypeDescription(Clash::class.java)
-////                .apply { putListPropertyType("proxies", Node::class.java) }
-//            ) }
-            .run {
-            with(Yaml(this).load(url) as Clash) {
-                println(this.proxies.map(Node::node)
-                    .filterIsInstance<V2ray>()
-                    .joinToString("|") {
+        with(Yaml(Constructor(Clash::class.java)).load(url) as Clash) {
+            println(this.proxies.map(Node::node)
+//                    .filterIsInstance<V2ray>()
+                .joinToString("|") {
 
                     it?.also { println(it.info()) }?.toUri() ?: ""
                 })
-            }
         }
+
     }
 }
